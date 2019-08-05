429722be6066fdcac6eb68afc1bd698272028fd6
Vasileios Souvatzis <souvatzisbill@gmail.com>
2019-08-05 17:23:30 +0300

    removed regex and utils helpers

--- helpers/utils.py
+++
@@ -1,5 +0,0 @@
-def is_year(text):
-    if int(text) < 1900 or int(text) > 2100:
-        return True
-    else:
-        return False
--- twitter_preprocessor.py
+++ twitter_preprocessor.py
@@ -3,7 +3,51 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk import re
-from helpers import regex, utils
+from helpers import regex
+
+
+def get_url_patern():
+    return re.compile(
+        r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))'
+        r'[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})')
+
+
+def get_emojis_pattern():
+    try:
+        # UCS-4
+        emojis_pattern = re.compile(u'([\U00002600-\U000027BF])|([\U0001f300-\U0001f64F])|([\U0001f680-\U0001f6FF])')
+    except re.error:
+        # UCS-2
+        emojis_pattern = re.compile(
+            u'([\u2600-\u27BF])|([\uD83C][\uDF00-\uDFFF])|([\uD83D][\uDC00-\uDE4F])|([\uD83D][\uDE80-\uDEFF])')
+    return emojis_pattern
+
+
+def get_hashtags_pattern():
+    return re.compile(r'#\w*')
+
+
+def get_single_letter_words_pattern():
+    return re.compile(r'(?<![\w\-])\w(?![\w\-])')
+
+
+def get_blank_spaces_pattern():
+    return re.compile(r'\s{2,}|\t')
+
+
+def get_twitter_reserved_words_pattern():
+    return re.compile(r'(RT|rt|FAV|fav|VIA|via)')
+
+
+def get_mentions_pattern():
+    return re.compile(r'@\w*')
+
+
+def is_year(text):
+    if int(text) < 1900 or int(text) > 2100:
+        return True
+    else:
+        return False
 
 
 class TwitterPreprocessor:
@@ -25,7 +69,7 @@ class TwitterPreprocessor:
             .remove_numbers()
 
     def remove_urls(self):
-        self.text = re.sub(pattern=regex.get_url_patern(), repl='', string=self.text)
+        self.text = re.sub(pattern=get_url_patern(), repl='', string=self.text)
         return self
 
     def remove_punctuation(self):
@@ -33,23 +77,23 @@ class TwitterPreprocessor:
         return self
 
     def remove_mentions(self):
-        self.text = re.sub(pattern=regex.get_mentions_pattern(), repl='', string=self.text)
+        self.text = re.sub(pattern=get_mentions_pattern(), repl='', string=self.text)
         return self
 
     def remove_hashtags(self):
-        self.text = re.sub(pattern=regex.get_hashtags_pattern(), repl='', string=self.text)
+        self.text = re.sub(pattern=get_hashtags_pattern(), repl='', string=self.text)
         return self
 
     def remove_twitter_reserved_words(self):
-        self.text = re.sub(pattern=regex.get_twitter_reserved_words_pattern(), repl='', string=self.text)
+        self.text = re.sub(pattern=get_twitter_reserved_words_pattern(), repl='', string=self.text)
         return self
 
     def remove_single_letter_words(self):
-        self.text = re.sub(pattern=regex.get_single_letter_words_pattern(), repl='', string=self.text)
+        self.text = re.sub(pattern=get_single_letter_words_pattern(), repl='', string=self.text)
         return self
 
     def remove_blank_spaces(self):
-        self.text = re.sub(pattern=regex.get_blank_spaces_pattern(), repl=' ', string=self.text)
+        self.text = re.sub(pattern=get_blank_spaces_pattern(), repl=' ', string=self.text)
         return self
 
     def remove_stopwords(self, extra_stopwords=None):
@@ -75,7 +119,7 @@ class TwitterPreprocessor:
         for text in text_list:
             if text.isnumeric():
                 if preserve_years:
-                    if utils.is_year(text):
+                    if is_year(text):
                         text_list.remove(text)
                 else:
                     text_list.remove(text)
